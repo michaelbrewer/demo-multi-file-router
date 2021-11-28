740f485eb40023035da415c73b6e3b5762b32c8c
Michael Brewer <michael.brewer@gyft.com>
2021-11-27 19:30:26 -0800

    chore: http-api version

--- src/users/main.py
+++ src/users/main.py
@@ -3,19 +3,19 @@ from typing import Dict
 from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.event_handler import ApiGatewayResolver
 from aws_lambda_powertools.event_handler.api_gateway import ProxyEventType
-from aws_lambda_powertools.logging.correlation_paths import API_GATEWAY_REST
+from aws_lambda_powertools.logging.correlation_paths import API_GATEWAY_HTTP
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 from .routers import health, users
 
 tracer = Tracer()
 logger = Logger()
-app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEvent)
+app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEventV2)
 app.include_router(health.router)
 app.include_router(users.router)
 
 
-@logger.inject_lambda_context(correlation_id_path=API_GATEWAY_REST)
+@logger.inject_lambda_context(correlation_id_path=API_GATEWAY_HTTP)
 @tracer.capture_lambda_handler
 def lambda_handler(event: Dict, context: LambdaContext):
     return app.resolve(event, context)
