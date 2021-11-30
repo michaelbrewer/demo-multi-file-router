77bb5d61cd882aac9a150eb9019ea61b34e63b7d
Michael Brewer <michael.brewer@gyft.com>
2021-11-30 13:28:57 -0800

    chore: add tracer

--- src/users/routers/health.py
+++ src/users/routers/health.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler.api_gateway import Router
+from aws_lambda_powertools.tracing import tracer
 
 router = Router()
 logger = Logger(child=True)
